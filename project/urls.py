da6094377ada6f2fad48de8ab6bfe32f608be28c
PiotrekSotor <piotrsotor@gmail.com>
2016-04-03 18:53:32 +0200

    Creating project

---
+++ project/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^proj_1/', include('proj_1.urls')),
+    url(r'^admin/', admin.site.urls),
+]

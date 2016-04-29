6de295956d2bd4b6fcd8b79bcc50d0405991675c
PiotrekSotor <piotrsotor@gmail.com>
2016-04-29 20:02:06 +0200

    begin of login interface

--- project/proj_2/urls.py
+++ project/proj_2/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-
+from django.conf import settings
 from . import views
 
+app_name = 'proj_2'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'login/$', views.login, name="login"),
 ]
--- project/proj_2/views.py
+++ project/proj_2/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.conf import settings
+from django.contrib.auth.decorators import  login_required
 
+
+@login_required(login_url='login')
 def index(request):
-    return HttpResponse("Hello world, this is index")
+    return HttpResponse("Hello world, this is index %s" % (settings.LOGIN_URL))
 
+def login(request):
+    if request.user.is_authenticated():
+        redirect('index.html')
+    return HttpResponse("Hello world, this is index")
 # Create your views here.

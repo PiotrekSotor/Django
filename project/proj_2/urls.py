0d819fc463ce9d671ce827705546377949443e9d
PiotrekSotor <piotrsotor@gmail.com>
2016-05-06 20:03:55 +0200

    finished forms, draft of aggregation

--- project/proj_2/urls.py
+++ project/proj_2/urls.py
@@ -4,7 +4,6 @@ import registration
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = 'proj_2'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/', views.login, name='login'),
@@ -16,15 +15,22 @@ urlpatterns = [
 
     url(r'^worker_edit/(?P<worker_id>[\S]+)/$', views.worker_edit, name='worker_edit'),
     url(r'^worker_erase/(?P<worker_id>[\S]+)/$', views.worker_erase, name='worker_erase'),
+    url(r'^worker_submit_form/$', views.worker_submit_form, name='worker_submit_form'),
     url(r'^workers/', views.workers, name='workers'),
 
     url(r'^job_edit/(?P<job_id>[\S]+)/$', views.job_edit, name='job_edit'),
     url(r'^job_erase/(?P<job_id>[\S]+)/$', views.job_erase, name='job_erase'),
+    url(r'^job_submit_form/$', views.job_submit_form, name='job_submit_form'),
     url(r'^jobs/', views.jobs, name='jobs'),
+
+    url(r'^aggregator/', views.aggregator, name='aggregator'),
+    url(r'^aggregator_form/', views.aggregator_form, name='aggregator_form'),
+
 
     url(r'^accounts/login/$', auth_views.login, name='login2'),
 
 ]
+app_name = 'proj_2'
 
 
 
--- project/proj_2/views.py
+++ project/proj_2/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, request, request
-from .forms import TaskForm
+from .forms import TaskForm, WorkerForm, JobForm, AggregatorForm
+from django.db import IntegrityError
+import datetime
+from datetime import date
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.template import Context
@@ -22,13 +25,13 @@ def login(request):
 # Create your views here.
 
 def tasks(request, message='', context={}):
-    tasks_to_render = Task.objects.all()
+    tasks_to_render = Task.objects.order_by("task_code")
     context['tasks'] = tasks_to_render
     if message:
         context['message'] = message
     if not 'form' in context:
-        context['form'] = TaskForm
-    print (context)
+        context['form'] = TaskForm()
+    print(context)
     return render(request, 'proj_2/tasks.html', context)
 
 
@@ -38,32 +41,28 @@ def task_submit_form(request):
         if form.is_valid():
             if 'id' in form.cleaned_data and not form.cleaned_data['id'] == '':
                 task = get_object_or_404(Task, pk=form.cleaned_data['id'])
-                task.wage = form.cleaned_data['wage']
-                task.save()
             else:
                 task = Task()
-                task.task_code = form.cleaned_data['task_code']
-                task.wage = form.cleaned_data['wage']
+                task.task_code = form.cleaned_data['task_code'].upper()
+            task.wage = form.cleaned_data['wage']
+            try:
                 task.save()
+            except IntegrityError as e:
+                return tasks(request, message='Błąd!   Powielenie kodu czynności')
             print(task.__str__())
-
 
             return tasks(request, 'Success')
         else:
-            print ('INVALID')
-            print (form.errors)
+            print('INVALID')
+            print(form.errors)
     return tasks(request, 'Failed')
 
 
 def task_edit(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     initials = {'task_code': task.task_code, 'wage': task.wage, 'id': task.id}
-    print(initials)
     form = TaskForm(initial=initials)
-    form.fields['task_code'].widget.attrs['readonly']=True
-    # form.task_code = task.task_code
-    # form.id = task.id
-    # form.wage = task.wage
+    form.fields['task_code'].widget.attrs['readonly'] = True
     context = {'form': form, 'edit': 1}
     return tasks(request, context=context)
 
@@ -74,9 +73,27 @@ def task_erase(request, task_id):
     return tasks(request, 'deleted task')
 
 
+def worker_submit_form(request):
+    if request.method == 'POST':
+        form = WorkerForm(request.POST)
+        if form.is_valid():
+            if 'id' in form.cleaned_data and not form.cleaned_data['id'] == '':
+                worker = get_object_or_404(Worker, pk=form.cleaned_data['id'])
+            else:
+                worker = Worker()
+            worker.name = form.cleaned_data['name'].upper()
+            worker.save()
+            return workers(request, 'Success')
+    return workers(request, 'Failed')
+
+
 def worker_edit(request, worker_id):
     worker = get_object_or_404(Worker, pk=worker_id)
-    return HttpResponse("worker edit %s" % (worker))
+    print(worker)
+    initials = {'name': worker.name, 'id': worker.id}
+    form = WorkerForm(initial=initials)
+    context = {'form': form, 'edit': 1}
+    return workers(request, context=context)
 
 
 def worker_erase(request, worker_id):
@@ -85,20 +102,70 @@ def worker_erase(request, worker_id):
     return workers(request, 'deleted worker')
 
 
-# def worker_submit_form(request):
-#
-
-def workers(request, message=''):
+def workers(request, message='', context={}):
     workers_to_render = Worker.objects.all()
-    context = {'workers': workers_to_render}
+    context['workers'] = workers_to_render
     if message:
         context['message'] = message
+    if not 'form' in context:
+        context['form'] = WorkerForm(initial={'date': datetime.date.today()})
+
     return render(request, 'proj_2/workers.html', context)
+
+
+def job_submit_form(request):
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        print(form.data)
+        if form.data['task_code_combobox']:
+            task_code_id = form.data['task_code_combobox']
+        elif form.data['task_code_field']:
+            task_code_id = 1
+        else:
+            return jobs(request, 'Brak kodu czynności')
+
+        if form.data['worker_combobox']:
+            worker_id = form.data['worker_combobox']
+        elif form.data['worker_field']:
+            worker_id = 1
+        else:
+            return jobs(request, 'Brak pracownika')
+
+        if form.data['count']:
+            count = form.data['count']
+        else:
+            return jobs(request, 'Brak ilości')
+
+        if form.data['date']:
+            date = form.data['date']
+        else:
+            date = datetime.date.today()
+
+        if 'id' in form.data and not form.data['id'] == '':
+            job = get_object_or_404(Job, pk=form.data['id'])
+            job.count = count
+            job.date = date
+            job.person_id = worker_id
+            job.task_code_id = task_code_id
+        else:
+            job = Job(task_code_id=task_code_id, person_id=worker_id, count=count, date=date)
+        job.save()
+
+        return jobs(request, 'Success')
+    return jobs(request, 'Failed')
 
 
 def job_edit(request, job_id):
     job = get_object_or_404(Job, pk=job_id)
-    return HttpResponse("job edit %s" % (job))
+    print(job)
+    initials = {'worker_combobox': job.person_id,
+                'task_code_combobox': job.task_code_id,
+                'count': job.count,
+                'date':job.date,
+                'id': job.id}
+    form = JobForm(initial=initials)
+    context = {'form': form, 'edit': 1}
+    return jobs(request, context=context)
 
 
 def job_erase(request, job_id):
@@ -107,9 +174,18 @@ def job_erase(request, job_id):
     return jobs(request, 'deleted job')
 
 
-def jobs(request, message=''):
+def jobs(request, message='', context={}):
     jobs_to_render = Job.objects.all()
-    context = {'jobs': jobs_to_render}
+    context['jobs'] = jobs_to_render
     if message:
         context['message'] = message
+    if not 'form' in context:
+        context['form'] = JobForm()
     return render(request, "proj_2/jobs.html", context)
+
+def aggregator(request, message='', context={}):
+    context['form'] = AggregatorForm(initial= {'startdate': date(date.today().year, date.today().month, 1),'enddate': date.today()})
+    return render(request, "proj_2/aggregator.html", context)
+
+def aggregator_form(request):
+    return aggregator(request)
